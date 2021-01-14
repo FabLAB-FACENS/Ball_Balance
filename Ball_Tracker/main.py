5fad68368b0fce13c6a8e74b131c6e828b083980
matheus.lopes <matheus.lopes@facens.br>
2021-01-14 15:12:20 -0300

    Ball tracker
    Update do código:
    
    O código gera 3 janelas com câmeras que reconhecem o objeto com a cor verde limão (HSV: l_b: 32,35,153; u_b: 78,255,255), reconhecendo seu centro e fazendo um círculo ao seu redor.
    É mostrado a posição do centro do objeto.

---
+++ Ball_Tracker/main.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import imutils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_b = np.array([32, 35, 153])
+    u_b = np.array([78, 255, 255])
+
+    mask = cv2.inRange(hsv, l_b, u_b)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.putText(res, str(center), center, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(frame, str(center), center, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("res", res)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
